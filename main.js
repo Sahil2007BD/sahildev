fd45e47087e3a90ab2f18e1c0723aa9769ef96c5
Your Name <sahiledu2007@gmail.com>
2026-03-16 17:51:13 +0600

    Add curson glow

--- main.js
+++ main.js
@@ -205,6 +205,16 @@ loader.style.display = "none";
 showPage();
 
 }
+// MOUSE GLOW EFFECT
+
+const glow = document.querySelector(".cursor-glow");
+
+document.addEventListener("mousemove", (e) => {
+
+glow.style.left = e.clientX + "px";
+glow.style.top = e.clientY + "px";
+
+});
 
 
 
